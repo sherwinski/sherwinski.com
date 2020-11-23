0b75371c35b6ceb5a45cc8bc2539f0e0cb8276d3
sherwinski <sherwin@imgix.com>
2020-11-23 14:53:57 -0800

    chore(cleanup): remove unused components and types
    Removes the following:
    - authors.ts & all references
    - alert component
    - footer component
    - authors from posts-(header|preview|hero)

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -2,14 +2,12 @@ import Avatar from './avatar'
 import DateFormatter from './date-formatter'
 import CoverImage from './cover-image'
 import Link from 'next/link'
-import Author from '../types/author'
 
 type Props = {
   title: string
   coverImage: string
   date: string
   excerpt: string
-  author: Author
   slug: string
 }
 
@@ -18,7 +16,6 @@ const PostPreview = ({
   coverImage,
   date,
   excerpt,
-  author,
   slug,
 }: Props) => {
   return (
@@ -35,7 +32,6 @@ const PostPreview = ({
         <DateFormatter dateString={date} />
       </div>
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
     </div>
   )
 }
