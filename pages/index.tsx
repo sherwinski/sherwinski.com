086e611ff320090ec37ceec5f8787ae9141a214b
sherwinski <sherwin@imgix.com>
2020-11-23 18:34:14 -0800

    remove heroPost and boilerplate Intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,24 +14,15 @@ type Props = {
 
 const Index = ({ allPosts }: Props) => {
   const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const morePosts = allPosts
   return (
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Sherwinski's Blog, built with Next.js</title>
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
