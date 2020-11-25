82e2f68ab309fb44e4187b881e315c7677fdfc17
sherwinski <sherwin@imgix.com>
2020-11-25 09:36:04 -0800

    add home page draft at /gallery

---
+++ pages/gallery.tsx
@@ -0,0 +1,25 @@
+
+export default function Gallery() {
+    return (
+        <>
+            <header id="header">
+                <div className="logo">
+                    {/* <span className="icon fa-gem"></span> */}
+                </div>
+                <div className="content">
+                    <div className="inner">
+                        <h1>Welcome 👋</h1>
+                        <p>Here's a space for my thoughts and random film photography.</p>
+                    </div>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="#blog">Blog</a></li>
+                        <li><a href="#gallery">Gallery</a></li>
+                        <li><a href="#github">GitHub</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
