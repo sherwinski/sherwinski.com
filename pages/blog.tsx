10328dbf62f17c3475d9a880489fc8ed8acdf1e6
sherwinski <sherwin@imgix.com>
2020-12-31 00:21:37 -0800

    switch routes for index and blog pages

---
+++ pages/blog.tsx
@@ -0,0 +1,47 @@
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPosts } from '../lib/api'
+import Head from 'next/head'
+import { CMS_NAME } from '../lib/constants'
+import Post from '../types/post'
+
+type Props = {
+  allPosts: Post[]
+}
+
+const Blog = ({ allPosts }: Props) => {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Sherwinski's Blog, built with Next.js</title>
+        </Head>
+        <Container>
+          <Intro />
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export default Blog
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,25 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../types/post'
 
-type Props = {
-  allPosts: Post[]
-}
-
-const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts
+export default function Index() {
   return (
-    <>
-      <Layout>
-        <Head>
-          <title>Sherwinski's Blog, built with Next.js</title>
-        </Head>
-        <Container>
-          <Intro />
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
+      <>
+          <header id="header">
+              <div className="logo">
+                  {/* <span className="icon fa-gem"></span> */}
+              </div>
+              <div className="content">
+                  <div className="inner">
+                      <h1>Welcome 👋</h1>
+                      <p>Here's a space for my thoughts and random film photography.</p>
+                  </div>
+              </div>
+              <nav>
+                  <ul>
+                      <li><a href="#blog">Blog</a></li>
+                      <li><a href="#gallery">Gallery</a></li>
+                      <li><a href="#github">GitHub</a></li>
+                  </ul>
+              </nav>
+          </header>
+      </>
   )
-}
-
-export default Index
-
-export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'coverImage',
-    'excerpt',
-  ])
-
-  return {
-    props: { allPosts },
-  }
 }
