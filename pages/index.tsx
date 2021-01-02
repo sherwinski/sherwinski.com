fdbca4491b86e2854699be563e28c439f7376f47
sherwinski <sherwin@imgix.com>
2021-01-02 14:53:36 -0800

    make nav links route to other pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,28 @@
+import { url } from "inspector";
 
 export default function Index() {
-  return (
-      <>
-          <header id="header">
-              <div className="logo">
-                  {/* <span className="icon fa-gem"></span> */}
-              </div>
-              <div className="content">
-                  <div className="inner">
-                      <h1>Welcome 👋</h1>
-                      <p>Here's a space for my thoughts and random film photography.</p>
-                  </div>
-              </div>
-              <nav>
-                  <ul>
-                      <li><a href="#blog">Blog</a></li>
-                      <li><a href="#gallery">Gallery</a></li>
-                      <li><a href="#github">GitHub</a></li>
-                  </ul>
-              </nav>
-          </header>
-      </>
-  )
+    return (
+        <>
+            {/* <div id="bg"> */}
+            <div id="sherwin">
+                <div>
+                    <header id="header">
+                        <div className="content">
+                            <div className="inner">
+                                <h1>Welcome 👋</h1>
+                                <p>Here's a space for my thoughts and random film photography.</p>
+                            </div>
+                        </div>
+                        <nav>
+                            <ul>
+                                <li><a href="/blog">Blog</a></li>
+                                <li><a href="/gallery">Gallery</a></li>
+                                <li><a href="https://github.com/sherwinski">GitHub</a></li>
+                            </ul>
+                        </nav>
+                    </header>
+                </div>
+            </div>
+        </>
+    )
 }
