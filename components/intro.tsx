bb7e4c3799a9d72e648e0e48c050e93c9225ee3d
sherwinski <sherwin@imgix.com>
2021-01-02 16:56:57 -0800

    remove headline from Intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,7 +4,6 @@ const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Welcome to my blog ✨
       </h4>
     </section>
   )
