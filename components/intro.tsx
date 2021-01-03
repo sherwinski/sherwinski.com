454e6c1d5a29dfa5ba6f5724ed2cc8bede931a87
sherwinski <sherwin@imgix.com>
2021-01-02 23:07:05 -0800

    add nav + anchor link to blog section

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,8 +1,12 @@
 import { CMS_NAME } from '../lib/constants'
 
-const Intro = () => {
+type Props = {
+  id?: string
+}
+
+const Intro = ({ id }: Props) => {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section id={id} className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
       </h4>
     </section>
