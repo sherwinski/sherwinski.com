12423119b03989a532b7e4cbc7ec5cc17b67ccca
sherwinski <sherwin@imgix.com>
2021-01-10 22:26:18 -0800

    prune unused vars and imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Container from "../components/container";
 import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
 import Intro from "../components/intro";
 import Layout from "../components/layout";
 import { getAllPosts } from "../lib/api";
 import Head from "next/head";
-import { CMS_NAME } from "../lib/constants";
 import Post from "../types/post";
 import Avatar from "../components/avatar";
 
@@ -14,7 +12,6 @@ type Props = {
 };
 
 const Blog = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0];
   const morePosts = allPosts;
   return (
     <>
