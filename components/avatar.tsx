b147ed46dab0d32f9a1e8d1d769a1f730051769c
sherwinski <sherwin@imgix.com>
2021-01-10 19:33:07 -0800

    refactor the Avatar component to use next/image

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,13 +1,21 @@
+import Image from 'next/image';
+
 type Props = {
   name?: string
   picture: string
-  id?: string
 }
 
-const Avatar = ({ name, picture, id }: Props) => {
+const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
-      <img src={picture} id={id} className="w-64 h-64 rounded-full" alt={name} />
+      <Image
+        src={picture}
+        className="w-64 h-64 rounded-full"
+        alt={name}
+        layout="fixed"
+        width={275}
+        height={275}
+      />
       <div className="text-xl font-bold">{name}</div>
     </div>
   )
