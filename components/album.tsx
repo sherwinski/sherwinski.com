dbef439a750982efa85591ce37902854175df3bc
sherwinski <sherwin@imgix.com>
2021-01-31 01:07:44 -0800

    add Luminous Gallery on gallery images

--- components/album.tsx
+++ components/album.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { LuminousGallery } from 'luminous-lightbox';
+import { useEffect } from 'react';
 
 type Path = {
     attributes: {
@@ -6,27 +8,36 @@ type Path = {
     }
 }
 type Props = {
-    paths: any
+    paths: any,
+    domain: string
 }
 
-export default function Album({ paths }: Props) {
+export default function Album({ paths, domain }: Props) {
+    useEffect(() => {
+        const images = document.querySelectorAll(".luminous-gallery");
+        const luminous = new LuminousGallery(images);
+    });
+
     return (
         <>
             <div id="gallery">            
                 <ul>
-                {paths
-                .filter((path: any) => path.attributes.origin_path != "/com/") // TODO: remove this line after this erroneous entity is removed from the imgix IM
-                .map((path: any) => (
-                        <li>
-                            <Image
-                                src={path.attributes.origin_path}
-                                layout="intrinsic"
-                                key={path.attributes.origin_path}
-                                width={330}
-                                height={500}
-                            />
-                        </li>    
-                ))}
+                {
+                    paths
+                    .filter((path: any) => path.attributes.origin_path != "/com/") // TODO: remove this line after this erroneous entity is removed from the imgix IM
+                    .map((path: any) => (
+                            <li>
+                                <a className="luminous-gallery" href={domain+path.attributes.origin_path+"?auto=format,compress&w=1200"}>
+                                    <Image
+                                        src={path.attributes.origin_path}
+                                        layout="fill"
+                                        objectFit="cover"
+                                    />
+                                </a>
+                            </li>
+                        )
+                    )
+                }
                 </ul>
             </div>
         </>
