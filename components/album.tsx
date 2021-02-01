9e05de8672a27d6fc2b4501d8f427d2f2a8f956c
sherwinski <sherwin@imgix.com>
2021-02-01 00:12:19 -0800

    resolve warning: missing unique key props

--- components/album.tsx
+++ components/album.tsx
@@ -30,7 +30,7 @@ export default function Album({ paths, domain }: Props) {
                     paths
                     .filter((path: any) => path.attributes.origin_path != "/com/") // TODO: remove this line after this erroneous entity is removed from the imgix IM
                     .map((path: any) => (
-                            <li className="gallery-images">
+                            <li className="gallery-images" key={path.attributes.origin_path}>
                                 <a className="luminous-gallery" href={domain+path.attributes.origin_path+"?auto=format,compress&w=1200"}>
                                     <Image
                                         src={path.attributes.origin_path}
